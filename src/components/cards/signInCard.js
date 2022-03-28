bb3e8ad1fe8ba7238864d6f197751e28579ece29
joao <joao.lobom@gmail.com>
2022-03-28 20:41:07 +0100

    Simple signIn form

--- src/components/cards/signInCard.js
+++ src/components/cards/signInCard.js
@@ -2,20 +2,24 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/Nav";
-import "./signInCard.css";
+import React, { useState } from "react";
 
 const SignInCard = () => {
-  return (
-    <div>
+  const [tab, setTab] = useState("signIn");
+  // const handleSubmit = (event) => {
+
+  // };
+  if (tab === "signIn") {
+    return (
       <Form className="d-flex justify-content-center">
         <Card style={{ width: "30%" }}>
           <Card.Header>
-            <Nav fill variant="tabs" defaultActiveKey="#first">
+            <Nav fill variant="tabs">
               <Nav.Item>
-                <Nav.Link href="#first">SignIn</Nav.Link>
+                <Nav.Link onClick={() => setTab("signIn")}>SignIn</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="#link">SignUp</Nav.Link>
+                <Nav.Link onClick={() => setTab("signUp")}>SignUp</Nav.Link>
               </Nav.Item>
             </Nav>
           </Card.Header>
@@ -24,21 +28,70 @@ const SignInCard = () => {
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
               </Form.Group>
             </Card.Text>
             <Card.Text>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="passwordSignIn" placeholder="Password" />
               </Form.Group>
             </Card.Text>
             <Button variant="primary">Submit</Button>
           </Card.Body>
         </Card>
       </Form>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <Form className="d-flex justify-content-center">
+        <Card style={{ width: "30%" }}>
+          <Card.Header>
+            <Nav fill variant="tabs">
+              <Nav.Item>
+                <Nav.Link onClick={() => setTab("signIn")}>SignIn</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={() => setTab("signUp")}>SignUp</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" />
+              </Form.Group>
+            </Card.Text>
+            <Card.Text>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Job</Form.Label>
+                <Form.Control type="job" placeholder="Enter your job" />
+              </Form.Group>
+            </Card.Text>
+            <Card.Text>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+            </Card.Text>
+            <Card.Text>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="passwordSignUp" placeholder="Password" />
+              </Form.Group>
+            </Card.Text>
+            <Card.Text>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="passwordConfirmation" placeholder="Password confirmation" />
+              </Form.Group>
+            </Card.Text>
+            <Button variant="primary">Submit</Button>
+          </Card.Body>
+        </Card>
+      </Form>
+    );
+  }
 };
 
 export default SignInCard;
