d67565c799ee3e00361891d71c5c7286fdc1d67a
joao <joao.lobom@gmail.com>
2022-03-28 19:00:41 +0100

    React router v6 changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./signInPage/signIn";
 import SignUp from "./signInPage/signUpPage/signUp";
 
 function App() {
   return (
     <Router>
-        <div>
+      <div>
         <Routes>
-          <Route path="/" exact component={<SignIn />} />
-          <Route path="/sign-up" exact component={<SignUp />} />
-          </Routes>
-        </div>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
