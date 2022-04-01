bd0f16f983b6bee8a8d5e5fdbbe3bcfa70b4e73d
joao <joao.lobom@gmail.com>
2022-04-01 10:29:54 +0100

    Change title

--- src/signInPage/signIn.js
+++ src/signInPage/signIn.js
@@ -12,7 +12,7 @@ const SignIn = () => {
           <h1>Welcome to decentralized working BETA</h1>
           <h4>
             The objective of this project is to enable Bitcoin earnings in a decentralized manner, without any company
-            structure, or bureaucracy behind
+            structure, bureaucracy, or KYC
           </h4>
           <p>
             If there is interest for this a project, I will improve it until it becomes as seamless as pushing a commit
