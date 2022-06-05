244332b31dcfd83b8c6a65b95531e0eed10dec71
joao <joao.lobom@gmail.com>
2022-06-05 15:53:04 +0100

    Small change to name

--- src/components/nav/navBar.js
+++ src/components/nav/navBar.js
@@ -21,7 +21,7 @@ const NavBar = (props) => {
     <div>
       <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Dework</Navbar.Brand>
+          <Navbar.Brand href="#home">Tallypeer</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
